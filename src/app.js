96aeb88a4f2d3344c9dc8616a44afad61cfc70c7
Hevit <linkinparknumb303@gmail.com>
2025-11-25 12:59:05 +0530

    initial

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// src/app.js
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello from acquisitions server!");
+});
+
+export default app;
